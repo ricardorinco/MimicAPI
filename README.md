893c9e92cac3b4ba8945c8d69a005f8689c9c5bd
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-08-09 13:22:35 -0300

    Commit inicial
    Criação da estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,22 @@
+# Mimic API
+
+Este é um projeto de uma api para um app de mimicas.
+
+![GitHub last commit (branch)](https://img.shields.io/github/last-commit/ricardorinco/mimicapi/master?label=LAST%20COMMIT%20&style=for-the-badge)
+
+## :rocket: Tecnologias
+
+Projeto desenvolvido com as seguintes tecnologias:
+
+- [.Net Core](https://dotnet.microsoft.com/)
+
+## :construction_worker: Como rodar
+
+``` bash
+ # Clonar repositório
+ $ git clone https://github.com/ricardorinco/mimicapi.git
+```
+
+## :memo: License
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
---
+++ src/Mimic.API/Startup.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Mimic.API
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(o => o.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
