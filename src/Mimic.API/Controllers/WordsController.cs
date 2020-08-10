d495c9b5d403b4f61ee91b6db4e9fcd4f2e176cb
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-08-10 07:31:31 -0300

    Criação da WordsController

---
+++ src/Mimic.API/Controllers/WordsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mimic.WebApi.Database.DataContext;
+using Mimic.WebApi.Models;
+using System.Linq;
+
+namespace Mimic.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WordsController : ControllerBase
+    {
+        private readonly MimicContext mimicContext;
+
+        public WordsController(MimicContext mimicContext)
+        {
+            this.mimicContext = mimicContext;
+        }
+
+        public IActionResult GetAll()
+        {
+            return Ok(mimicContext.Words);
+        }
+
+        public IActionResult Get(int id)
+        {
+            return Ok(mimicContext.Words.Where(x => x.Id == id));
+        }
+
+        public IActionResult Add(Word word)
+        {
+            mimicContext.Words.Add(word);
+
+            return Ok();
+        }
+
+        public IActionResult Update(Word word)
+        {
+            mimicContext.Words.Update(word);
+
+            return Ok();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            mimicContext.Remove(mimicContext.Words.Find(id));
+
+            return Ok();
+        }
+    }
+}
