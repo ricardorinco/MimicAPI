d7334594144e5e5bf7d3abfa7cd95faa30fe2180
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-08-11 07:16:50 -0300

    Ajustes na controller Words

--- src/Mimic.API/Controllers/WordsController.cs
+++ src/Mimic.API/Controllers/WordsController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Mimic.WebApi.Database.DataContext;
 using Mimic.WebApi.Models;
+using System;
 using System.Linq;
 
 namespace Mimic.WebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
     public class WordsController : ControllerBase
     {
         private readonly MimicContext mimicContext;
@@ -16,32 +16,49 @@ namespace Mimic.WebApi.Controllers
             this.mimicContext = mimicContext;
         }
 
+        [Route("")]
+        [HttpGet]
         public IActionResult GetAll()
         {
             return Ok(mimicContext.Words);
         }
 
+        [Route("{id}")]
+        [HttpGet]
         public IActionResult Get(int id)
         {
             return Ok(mimicContext.Words.Where(x => x.Id == id));
         }
 
-        public IActionResult Add(Word word)
+        [Route("")]
+        [HttpPost]
+        public IActionResult Add([FromBody]Word word)
         {
+            word.CreatedAt = DateTime.Now;
             mimicContext.Words.Add(word);
+            mimicContext.SaveChanges();
 
             return Ok();
         }
 
-        public IActionResult Update(Word word)
+        [Route("{id}")]
+        [HttpPut]
+        public IActionResult Update(int id, [FromBody]Word word)
         {
+            word.Id = id;
+            word.UpdatedAt = DateTime.Now;
             mimicContext.Words.Update(word);
+            mimicContext.SaveChanges();
 
             return Ok();
         }
 
+        [Route("{id}")]
+        [HttpDelete]
         public IActionResult Delete(int id)
         {
+            var word = mimicContext.Words.Find(id);
+            word.Active = false;
             mimicContext.Remove(mimicContext.Words.Find(id));
 
             return Ok();
