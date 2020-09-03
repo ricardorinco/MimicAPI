df11bdd9f61f94fdecd4a4ce4892ed6fd5bb3985
Ricardo Rinco <ricardo.rinco@hotmail.com>
2020-09-02 21:33:06 -0300

    Implementação do IValidationHandler

---
+++ src/Mimic.Application/Interfaces/IValidationHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace Mimic.Application.Interfaces
+{
+    public interface IValidationHandler<RuleDto>
+    {
+        IValidationHandler<RuleDto> Next { get; set; }
+
+        dynamic Apply(RuleDto ruleDto);
+    }
+}
